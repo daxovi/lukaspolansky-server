6a9f6fbddb125d98eac9017e35114c53d8acbe7c
The Avarga <theavarga@the-macbook-air.home>
2021-04-29 13:36:46 +0200

    another update

---
+++ models/material.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const material = new mongoose.Schema({
+name:{
+    type:String,
+}
+});
+module.exports = mongoose.model("Material", material);
--- server.js
+++ server.js
@@ -2,8 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const db = require("./databaze/connect");
-
+const getMaterials = require("./routes/GET/getMaterial");
+const saveMaterial = require("./routes/POST/saveMaterial");
 db.connect();
+
+/**
+ * Middleware
+ * Povolme přijímat JSON z frontendu
+ */
+ app.use(express.json({extended:false}));
+ app.use(express.text({extended:false}));
+
+/**
+ * Routy - GET
+ */
+app.use("/",getMaterials);
+/**
+ * Routy - POST
+ */
+app.use("/", saveMaterial);
+
+
 app.get("/", (request,response) => {
     response.send("Hlavní stránka");
 });
