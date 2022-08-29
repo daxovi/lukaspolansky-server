c0a7273926bd74957686ad62fae2070fa68b28ae
daxovi <48390551+daxovi@users.noreply.github.com>
2022-08-29 11:21:23 +0000

    user model optimalization

--- models/user.js
+++ models/user.js
@@ -1,74 +1,85 @@
 const mongoose = require("mongoose");
 
+const lesson = new mongoose.Schema({
+    title: String,
+    file: String,
+    completed: Number,
+    date: Date,
+    eval: [Number]
+})
+
 const user = new mongoose.Schema({
     name: {
         type: String,
     },
     course:
-        [{
-            title: String,
-            file: String,
-            completed: Number,
-            date: Date,
-            eval: [Number]
-
-        },
-        {
-            title: String,
-            file: String,
-            completed: Number,
-            date: Date,
-            eval: [Number]
-        },
-        {
-            title: String,
-            file: String,
-            completed: Number,
-            date: Date,
-            eval: [Number]
-        },
-        {
-            title: String,
-            file: String,
-            completed: Number,
-            date: Date,
-            eval: [Number]
-        },
-        {
-            title: String,
-            file: String,
-            completed: Number,
-            date: Date,
-            eval: [Number]
-        },
-        {
-            title: String,
-            file: String,
-            completed: Number,
-            date: Date,
-            eval: [Number]
-        },
-        {
-            title: String,
-            file: String,
-            completed: Number,
-            date: Date,
-            eval: [Number]
-        },
-        {
-            title: String,
-            file: String,
-            completed: Number,
-            date: Date,
-            eval: [Number]
-        },
-        {
-            title: String,
-            file: String,
-            completed: Number,
-            date: Date,
-            eval: [Number]
-        }],
+        [lesson
+            /*
+            {
+                title: String,
+                file: String,
+                completed: Number,
+                date: Date,
+                eval: [Number]
+            },
+            {
+                title: String,
+                file: String,
+                completed: Number,
+                date: Date,
+                eval: [Number]
+            },
+            {
+                title: String,
+                file: String,
+                completed: Number,
+                date: Date,
+                eval: [Number]
+            },
+            {
+                title: String,
+                file: String,
+                completed: Number,
+                date: Date,
+                eval: [Number]
+            },
+            {
+                title: String,
+                file: String,
+                completed: Number,
+                date: Date,
+                eval: [Number]
+            },
+            {
+                title: String,
+                file: String,
+                completed: Number,
+                date: Date,
+                eval: [Number]
+            },
+            {
+                title: String,
+                file: String,
+                completed: Number,
+                date: Date,
+                eval: [Number]
+            },
+            {
+                title: String,
+                file: String,
+                completed: Number,
+                date: Date,
+                eval: [Number]
+            },
+            {
+                title: String,
+                file: String,
+                completed: Number,
+                date: Date,
+                eval: [Number]
+            }
+            */
+        ],
 });
 
 module.exports = mongoose.model("User", user);
